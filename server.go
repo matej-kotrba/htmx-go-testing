94791e7f5d4b28cb2c68fac6ac696081dc0f42da
matej-kotrba <matejkotrba.skola@gmail.com>
2024-10-21 19:52:58 +0200

    update: added post route

--- server.go
+++ server.go
@@ -32,8 +32,13 @@ func main() {
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index.html", count)
+	})
+
+	e.POST("/add", func(c echo.Context) error {
 		count.Count++
 		return c.Render(200, "index.html", count)
 	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
